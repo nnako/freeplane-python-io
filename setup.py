2e36d39932412496815e96575be208278ad46abd
nnako <nnako@web.de>
2016-03-23 20:31:52 +0100

    added ingredients to make this module installable

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='freeplane', version='1.0.0',
+    py_modules='freeplane',
+    author='nnako',
+    author_email='nnako@web.de',
+    url='www.nnako.de',
+    description='provide read and write access to freeplane mindmapping files',
+)
