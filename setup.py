684aec676c80a8f4d76285b4c0968a875e5855a9
nnako <nnako@web.de>
2022-09-28 21:20:00 +0200

    FIX: better information for deployment into PyPi

--- setup.py
+++ setup.py
@@ -1,11 +1,28 @@
 from distutils.core import setup
 
+# get long description for display within PyPi
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+# do the settings for PyPi
 setup(
     name='freeplane',
     version='0.1.1',
     py_modules=['freeplane'],
     author='nnako',
     author_email='nnako@web.de',
-    url='www.nnako.de',
-    description='provide read and write access to freeplane mindmapping files',
+    url="https://github.com/nnako/freeplane-python-io",
+    description='provide create, read, update and delete of freeplane nodes via file access',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        "html2text ~= 2020.1.16",
+        ]
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
+        "Operating System :: OS Independent",
+        }
 )
